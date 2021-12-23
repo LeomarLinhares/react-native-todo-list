185085d6481ee6a0e9e453373f16c2ce90f8a721
Leomar Linhares <leomar_linhares@hotmail.com>
2021-12-23 11:25:45 -0300

    :construction: cria função para puxar informações na storage do celular

--- src/contexts/GlobalContext.jsx
+++ src/contexts/GlobalContext.jsx
@@ -1,16 +1,29 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const GlobalProvider = createContext({});
 
 export default function GlobalContext({ children }) {
   const [taskList, setTaskList] = useState([]);
+  const [isLoadingData, setIsLoadingData] = useState(false);
   const [addTaskModalIsVisible, setAddTaskModalIsVisible] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await AsyncStorage.getItem('@all_tasks');
+      return response !== null ? JSON.parse(response) : null;
+    };
+    setIsLoadingData(true);
+    setTaskList(getData());
+    setIsLoadingData(false);
+  }, [])
 
   const GlobalState = {
     taskList,
     setTaskList,
     addTaskModalIsVisible,
     setAddTaskModalIsVisible,
+    isLoadingData,
   };
 
   return (
