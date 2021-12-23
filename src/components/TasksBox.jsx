3ccad9be7c59e6b0d97bf91b5274e42f88b6aaee
Leomar Linhares <leomar_linhares@hotmail.com>
2021-12-23 09:02:32 -0300

    :construction: adiciona mensagem quando não houver tarefas

--- src/components/TasksBox.jsx
+++ src/components/TasksBox.jsx
@@ -1,20 +1,45 @@
 import React, { useContext } from 'react';
-import { View, FlatList } from 'react-native';
+import { View, FlatList, StyleSheet, Dimensions, Text } from 'react-native';
 import { GlobalProvider } from '../contexts/GlobalContext';
 import TaskCard from './TaskCard';
+
+const screen = Dimensions.get('screen');
+const window = Dimensions.get('window');
 
 export default function TasksBox() {
   const { taskList } = useContext(GlobalProvider);
 
-  const renderTask = ({ item }) => <TaskCard task={ item.title } />;
+  const renderTask = ({ item }) => <TaskCard task={ item.task } />;
 
   return (
-    <View>
-      <FlatList
-        data={ taskList }
-        renderItem={ renderTask }
-        keyExtractor={ item => item.id }
-      />
+    <View style={ styles.container }>
+      {
+        taskList.length
+          ? <FlatList
+              data={ taskList }
+              renderItem={ renderTask }
+              keyExtractor={ item => item.id }
+            />
+          : <View style={ styles.noTasksContainer }>
+              <Text>Toque em "+" para adicionar tarefas.</Text>
+            </View>
+      }
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    width: screen.width - 60,
+    minHeight: window.height - 50,
+  },
+  noTasksContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '90%',
+  },
+});
