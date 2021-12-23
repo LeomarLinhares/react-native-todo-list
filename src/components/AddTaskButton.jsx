1d3e781548c7d8711d58437b9d963e102b3bbe8b
Leomar Linhares <leomar_linhares@hotmail.com>
2021-12-23 07:50:20 -0300

    :sparkles: cria o botão que abre o modal de adição de tarefas AddTaskButton.jsx

---
+++ src/components/AddTaskButton.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useContext } from 'react';
+import { Pressable, StyleSheet, Dimensions } from 'react-native';
+import { GlobalProvider } from '../contexts/GlobalContext';
+import IconPlus from '../Icons/IconPlus';
+
+const screen = Dimensions.get('screen');
+
+const buttonDimensionLimiter = (calc) => calc > 75 ? 75 : calc;
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black',
+    width: buttonDimensionLimiter(screen.width / 5.6),
+    height: buttonDimensionLimiter(screen.width / 5.6),
+    borderRadius: 50,
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    zIndex: 1,
+  },
+  text: {
+    color: 'white',
+    fontSize: 40,
+  },
+});
+
+export default function AddTaskButton() {
+  const [buttonDimensions, setButtonDimensions] = useState({});
+  const { addTaskModalIsVisible, setAddTaskModalIsVisible } = useContext(GlobalProvider);
+
+  return (
+    <Pressable
+      style={ styles.container }
+      onLayout={ ({ nativeEvent: { layout } }) =>  setButtonDimensions(layout) }
+      onPress={ () => setAddTaskModalIsVisible(!addTaskModalIsVisible) }
+    >
+      <IconPlus />
+    </Pressable>
+  )
+}
