b012493de76428f886d529bf16e6250c2f406d39
Leomar Linhares <leomar_linhares@hotmail.com>
2021-12-23 09:27:46 -0300

    :construction: style: TaskCard

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -5,16 +5,24 @@ const styles = StyleSheet.create({
   container: {
     display: 'flex',
     flexDirection: 'row',
-    height: 40,
+    height: 60,
     alignItems: 'center',
+    borderWidth: 1,
+    borderRadius: 10,
+    marginTop: 10,
   },
+  textStyle: {
+    marginHorizontal: 10,
+  }
 });
 
 export default function TaskCard({ task, fatherLayoutInfo, id }) {
   const { width } = fatherLayoutInfo;
   return (
-    <View style={ { ...styles.container, width: width - 10 } }>
-      <Text>{ task }</Text>
+    <View
+      style={ { ...styles.container, width: width - 10 } }
+    >
+      <Text style={ styles.textStyle }>{ task }</Text>
     </View>
   )
 }
